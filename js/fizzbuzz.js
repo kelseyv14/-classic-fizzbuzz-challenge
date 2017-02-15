7ba2eb3c126b10704bce997d81fffa5eb4cd2db2
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-15 10:12:16 -0500

    class edit

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -7,8 +7,9 @@ function FizzBuzz(n) {
    
 }
 // places variables as rules for the function...still confused about i++...is it 1+1+1? 3? so maybe this means i is 1, 1 is less than or equal to n; 3?
-    for (var i = 1; i <= n; i++) 
-    {} 
+    for (var i = 1; i <= n; i++) {
+
+    } 
     // says if 1 can be divisable by three or five the output should read FizzBuzz
         if (i % 3 === 0 && i % 5 === 0) {
             output += 'FizzBuzz';
@@ -31,10 +32,8 @@ function FizzBuzz(n) {
     return output;
         }
 
+
 // tests
-FizzBuzz(15);
-
-
 
 avow('FizzBuzz 1 should be .', '.' === FizzBuzz(1), '.', FizzBuzz(1));
 avow('FizzBuzz 2 should be ..', '..' === FizzBuzz(2), '..', FizzBuzz(2));
