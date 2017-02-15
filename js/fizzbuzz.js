4f02660946cf80883e06f01645537058e92379f7
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-14 23:42:06 -0500

    finished js correctly...?

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -4,10 +4,11 @@ var avow = require('avow');
 // names the function, setting up the equation
 function FizzBuzz(n) {
     var output = '';
-    return output;
+   
 }
 // places variables as rules for the function...still confused about i++...is it 1+1+1? 3? so maybe this means i is 1, 1 is less than or equal to n; 3?
-    for (var i = 1; i <= n; i++) {
+    for (var i = 1; i <= n; i++) 
+    {} 
     // says if 1 can be divisable by three or five the output should read FizzBuzz
         if (i % 3 === 0 && i % 5 === 0) {
             output += 'FizzBuzz';
@@ -22,14 +23,14 @@ function FizzBuzz(n) {
         }
 // if it is not divisable by either, the output is a period
         else {
-            output +='.';
-        }
-    }
+        output +='.'; }
+        
+    
 
 //  the special command that tells it not to loop ?
     return output;
+        }
 
-}
 // tests
 FizzBuzz(15);
 
